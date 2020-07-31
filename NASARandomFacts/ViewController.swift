b34f9f4847660bea74ab00be0e34f63d9e44e4c3
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-07-31 06:52:29 +0200

    Create Simple API Requests
    The NASA random picture of the day was chosen as the API of choice and tested within the app.

--- NASARandomFacts/ViewController.swift
+++ NASARandomFacts/ViewController.swift
@@ -11,11 +11,34 @@ import UIKit
 class ViewController: UIViewController {
 
   @IBOutlet weak var tableView: UITableView!
+  var randomItems = [RandomFacts](){
+    didSet {
+      DispatchQueue.main.async {
+        self.tableView.reloadData()
+      }
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    let randomFactRequest = RandomFactsRequest(dateQuery: "2015-09-09")
+    randomFactRequest.getSearchResult{result in
+      self.randomItems.append(result)
+    }
   }
+}
 
-
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return randomItems.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+    
+    let randomFact = randomItems[indexPath.row]
+    cell.textLabel?.text = randomFact.title
+    return cell
+  }
 }
 
