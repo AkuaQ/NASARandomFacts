1b2565944b663a1b382903119147a2e283f5072b
Akua <AAfraneOkese@jhb.dvt.co.za>
2020-07-31 06:23:50 +0200

    Created New Project

---
+++ NASARandomFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NASARandomFacts
+//
+//  Created by Akua Afrane-Okese on 2020/07/31.
+//  Copyright © 2020 Akua Afrane-Okese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
